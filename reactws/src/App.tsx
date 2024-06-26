435e166cd7af4e430720493ea00c923db33713af
Your New Username <sahupravin960@gmail.com>
2024-06-26 16:11:14 +0530

    testing video sync via webssocket

--- reactws/src/App.tsx
+++ reactws/src/App.tsx
@@ -1,29 +1,65 @@
-import { useEffect, useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 interface chat {
   msg: string;
 }
-function App() {
-  const [socket, newSocket] = useState<null | WebSocket>();
 
+function App() {
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const [socket, newSocket] = useState<null | WebSocket>(null);
   const [chat, setChat] = useState("");
   const [chatHistory, setHistory] = useState<chat[]>([]);
+  const [play, setPlay] = useState(false);
+  const playerRef = useRef<any>(null);
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    playerRef.current = event.target;
+  };
+
+  const onPlayerPlay: YouTubeProps["onPlay"] = (event) => {
+    console.log("play");
+
+    socket?.send("play");
+  };
+
+  const onPlayerPause: YouTubeProps["onPause"] = (event) => {
+    console.log("pause");
+    socket?.send("pause");
+  };
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
     ws.onopen = () => {
       console.log("connection established");
-      // ws.send("heelo server");
     };
     ws.onmessage = (message) => {
       console.log("Message received:", message.data);
+      if (message.data === "play") {
+        setPlay(true);
+      } else if (message.data === "pause") {
+        setPlay(false);
+      }
       setHistory((prevHistory) => [...prevHistory, { msg: message.data }]);
     };
     newSocket(ws);
     return () => ws.close();
-  }, []);
+  }, [play]);
+  useEffect(() => {
+    if (play) {
+      console.log("played bro");
+      playerRef.current?.playVideo();
+    } else {
+      playerRef.current?.pauseVideo();
+    }
+  }, [play]);
 
   if (!socket) {
     return <div>connecting to server....</div>;
@@ -39,34 +75,32 @@ function App() {
           }}
         >
           {chatHistory.map((msg, index) => (
-            <>
-              <span
-                style={{
-                  margin: "13",
-                  padding: "2px",
-                }}
-                key={index}
-              >
-                {msg.msg}
-              </span>
+            <span
+              key={index}
+              style={{
+                margin: "13",
+                padding: "2px",
+              }}
+            >
+              {msg.msg}
               <br />
-            </>
+            </span>
           ))}
         </div>
-        <input
-          type="text"
-          onChange={(e) => {
-            setChat(e.target.value);
-          }}
+        <input type="text" onChange={(e) => setChat(e.target.value)} />
+        <button onClick={() => socket?.send(chat)}>Send</button>
+      </div>
+      <audio controls>
+        <source src="./song.ogg" type="audio/ogg" />
+      </audio>
+      <div>
+        <YouTube
+          videoId="2g811Eo7K8U"
+          opts={opts}
+          onReady={onPlayerReady}
+          onPlay={onPlayerPlay}
+          onPause={onPlayerPause}
         />
-
-        <button
-          onClick={() => {
-            socket?.send(chat);
-          }}
-        >
-          Send
-        </button>
       </div>
     </>
   );
--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -11,18 +11,20 @@ const httpserver = app.listen(8080, () => {
 
 const wss = new WebSocketServer({ server: httpserver });
 
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+wss.on(
+  "connection",
+  function connection(ws: import("ws"), request: any, client: any) {
+    ws.on("error", console.error);
 
-  ws.on("message", function message(data, isbinery) {
-    ws.getMaxListeners()
-    ///here you will write you logic for application it could be sending to server sendin data form server and room server etc.
-    wss.clients.forEach(function each(clients) {
-      if (clients.readyState == WebSocket.OPEN) {
-        clients.send(data, { binary: isbinery });
-        console.log("someone has sent " + data);
-      }
+    ws.on("message", function message(data, isbinery) {
+      console.log(`Received message ${data} from user ${client}`);
+      ///here you will write you logic for application it could be sending to server sendin data form server and room server etc.
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data, { binary: isbinery });
+        }
+      });
     });
-  });
-  ws.send("Hello! Message From Server!!");
-});
+    ws.send("Hello! Message From Server!!");
+  }
+);
